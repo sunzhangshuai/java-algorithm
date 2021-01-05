47f19546179cdb70c23a205aa957b8ffe3b49c7f
sunchen <sunchen@trawe.cn>
2021-01-05 23:03:42 +0800

    算法初始化

---
+++ normal/src/main/java/com/normal/m153/FindMin.java
@@ -0,0 +1,25 @@
+package com.normal.m153;
+
+/**
+ * FindMin:
+ *
+ * @author sunchen
+ * @date 2021/1/5 10:45 ÏÂÎç
+ */
+public class FindMin {
+    public int handler(int[] nums){
+        int low = 0;
+        int high = nums.length-1;
+        while (low < high) {
+            int middle = (low+high)/2;
+            if (nums[middle] > nums[high]) {
+                low = middle + 1;
+            } else if (nums[middle] < nums[high]) {
+                high = middle;
+            } else {
+                high--;
+            }
+        }
+        return nums[low];
+    }
+}
