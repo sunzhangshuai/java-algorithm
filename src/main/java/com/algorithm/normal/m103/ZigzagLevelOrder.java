aaa9a4e721a5ab368c86b05bd4f8f53511b56c1f
sunchen <sunchen@trawe.cn>
2021-01-23 19:11:37 +0800

    二叉树的锯齿形层序遍历

--- src/main/java/com/algorithm/normal/m103/ZigzagLevelOrder.java
+++ src/main/java/com/algorithm/normal/m103/ZigzagLevelOrder.java
@@ -7,6 +7,7 @@ import java.util.Queue;
 /**
  * ZigzagLevelOrder: 二叉树的锯齿形层序遍历
  * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ *
  * @author sunchen
  * @date 2021/1/23 6:19 下午
  */
@@ -19,7 +20,7 @@ public class ZigzagLevelOrder {
         Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
         nodeQueue.offer(root);
         boolean left = true;
-        while (!nodeQueue.isEmpty()){
+        while (!nodeQueue.isEmpty()) {
             int size = nodeQueue.size();
             LinkedList<Integer> list = new LinkedList<>();
             for (int i = 0; i < size; i++) {
