81709a0d42e8546e421a89b61a1eeb9373c6e840
sunchen <sunchen@trawe.cn>
2021-01-23 18:42:50 +0800

    二叉树的锯齿形层序遍历

---
+++ src/main/java/com/algorithm/normal/m103/TreeNode.java
@@ -0,0 +1,16 @@
+package com.algorithm.normal.m103;
+
+/**
+ * TreeNode:
+ *
+ * @author sunchen
+ * @date 2021/1/23 6:19 下午
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/algorithm/normal/m103/ZigzagLevelOrder.java
@@ -0,0 +1,44 @@
+package com.algorithm.normal.m103;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * ZigzagLevelOrder: 二叉树的锯齿形层序遍历
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * @author sunchen
+ * @date 2021/1/23 6:19 下午
+ */
+public class ZigzagLevelOrder {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.offer(root);
+        boolean left = true;
+        while (!nodeQueue.isEmpty()){
+            int size = nodeQueue.size();
+            LinkedList<Integer> list = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = nodeQueue.poll();
+                if (left) {
+                    list.addLast(node.val);
+                } else {
+                    list.addFirst(node.val);
+                }
+                if (node.left != null) {
+                    nodeQueue.offer(node.left);
+                }
+                if (node.right != null) {
+                    nodeQueue.offer(node.right);
+                }
+            }
+            left = !left;
+            result.add(list);
+        }
+        return result;
+    }
+}
