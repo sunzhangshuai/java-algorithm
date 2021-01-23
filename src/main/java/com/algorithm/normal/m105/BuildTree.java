3a2b73dfd78d5088b761c070bff0cd8a0eea0433
sunchen <sunchen@trawe.cn>
2021-01-24 00:00:52 +0800

    从前序与中序遍历序列构造二叉树

---
+++ src/main/java/com/algorithm/normal/m105/BuildTree.java
@@ -0,0 +1,38 @@
+package com.algorithm.normal.m105;
+
+/**
+ * BuildTree: 从前序与中序遍历序列构造二叉树
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ * 例如，给出
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ *
+ * @author sunchen
+ * @date 2021/1/23 11:33 下午
+ */
+public class BuildTree {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return toBuild(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode toBuild(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) {
+            return null;
+        }
+        //获取根
+        int root = preorder[preStart];
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root) {
+                TreeNode treeNode = new TreeNode(root);
+                //获取左子树的先序和中序，创建左子树
+                treeNode.left = toBuild(preorder, preStart + 1, preStart + i - inStart, inorder, inStart, i - 1);
+                //获取右子树的先序和中序，创建右子树
+                treeNode.right = toBuild(preorder, preStart + i - inStart + 1, preEnd, inorder, i + 1, inEnd);
+                return treeNode;
+            }
+        }
+        return null;
+    }
+}
