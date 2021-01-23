6309f7bb16d48ceacf321d6c301b663ac6e86af3
sunchen <sunchen@trawe.cn>
2021-01-23 23:03:11 +0800

    完全平方数

---
+++ src/main/java/com/algorithm/normal/m279/NumSquares.java
@@ -0,0 +1,37 @@
+package com.algorithm.normal.m279;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * NumSquares:完全平方数
+ * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ * @author sunchen
+ * @date 2021/1/23 10:29 下午
+ */
+public class NumSquares {
+    Set<Integer> sqrt = new HashSet();
+    public boolean is_divide_by(int n, int count){
+        if (count == 1) {
+            return sqrt.contains(n);
+        }
+        for (int i = 1; i*i <= n; i++) {
+            if (is_divide_by(n - i*i, count-1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int numSquares(int n) {
+        for (int i = 1; i*i <=n; i++) {
+            sqrt.add(i*i);
+        }
+        int count = n;
+        for (int i = 1; i <= count; i++) {
+            if(is_divide_by(n,i)){
+                return i;
+            }
+        }
+        return count;
+    }
+}
--- src/main/java/com/algorithm/normal/m895/FreqStack.java
+++ src/main/java/com/algorithm/normal/m895/FreqStack.java
@@ -1,25 +1,29 @@
 package com.algorithm.normal.m895;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
 /**
  * FreqStack: 最大频率栈
  * 实现 FreqStack，模拟类似栈的数据结构的操作的一个类。
- *
+ * <p>
  * FreqStack 有两个函数：
- *
+ * <p>
  * push(int x)，将整数 x 推入栈中。
  * pop()，它移除并返回栈中出现最频繁的元素。
  * 如果最频繁的元素不只一个，则移除并返回最接近栈顶的元素。
+ *
  * @author sunchen
  * @date 2021/1/23 7:29 下午
  */
 public class FreqStack {
-    Map<Integer,Integer> freq;
+    Map<Integer, Integer> freq;
     Map<Integer, Stack<Integer>> group;
     int maxFreq = 0;
+
     public FreqStack() {
         freq = new HashMap<>();
         group = new HashMap<>();
@@ -27,12 +31,12 @@ public class FreqStack {
     }
 
     public void push(int x) {
-        int f = freq.getOrDefault(x,0)+1;
-        freq.put(x,f);
+        int f = freq.getOrDefault(x, 0) + 1;
+        freq.put(x, f);
         if (f > maxFreq) {
             maxFreq = f;
         }
-        group.computeIfAbsent(f, z->new Stack<>()).push(x);
+        group.computeIfAbsent(f, z -> new Stack<>()).push(x);
     }
 
     public int pop() {
