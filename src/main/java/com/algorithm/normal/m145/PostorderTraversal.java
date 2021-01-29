0b9c8ff905a68f5a13d0de3ff49ecb023186ef04
sunchen <sunchen@trawe.cn>
2021-01-29 23:35:44 +0800

    二叉树后序遍历

--- src/main/java/com/algorithm/normal/m145/PostorderTraversal.java
+++ src/main/java/com/algorithm/normal/m145/PostorderTraversal.java
@@ -1,11 +1,13 @@
 package com.algorithm.normal.m145;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * PostorderTraversal:
- *
+ * 二叉树的后序遍历
  * @author sunchen
  * @date 2021/1/29 10:29 下午
  */
@@ -15,6 +17,23 @@ public class PostorderTraversal {
             return new ArrayList<>();
         }
         List<Integer> result = new ArrayList<>();
-        return null;
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev){
+                result.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return result;
     }
 }
