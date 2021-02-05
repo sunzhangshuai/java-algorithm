0d2291937589533bb60237f3aee561495ac6edd9
sunchen <sunchen@trawe.cn>
2021-02-06 02:37:18 +0800

    螺旋矩阵

--- src/main/java/com/algorithm/normal/m59/GenerateMatrix.java
+++ src/main/java/com/algorithm/normal/m59/GenerateMatrix.java
@@ -1,15 +1,14 @@
 package com.algorithm.normal.m59;
 
 /**
- * GenerateMatrix:
+ * GenerateMatrix: 螺旋矩阵
  * 给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
- *
  * 输入: 3
  * 输出:
  * [
- *  [ 1, 2, 3 ],
- *  [ 8, 9, 4 ],
- *  [ 7, 6, 5 ]
+ * [ 1, 2, 3 ],
+ * [ 8, 9, 4 ],
+ * [ 7, 6, 5 ]
  * ]
  *
  * @author sunchen
@@ -18,11 +17,11 @@ package com.algorithm.normal.m59;
 public class GenerateMatrix {
     public int[][] generateMatrix(int n) {
         int left = 0;
-        int right = n-1;
+        int right = n - 1;
         int up = 0;
-        int down = n-1;
+        int down = n - 1;
         int value = 1;
-        int result[][] = new int[n][n];
+        int[][] result = new int[n][n];
         while (up <= down && left <= right) {
             for (int i = left; i <= right; i++) {
                 result[up][i] = value++;
@@ -32,11 +31,11 @@ public class GenerateMatrix {
                 result[i][right] = value++;
             }
             right--;
-            for (int i = right; i >= left && value - 1 < Math.pow(n,2); i--) {
+            for (int i = right; i >= left && value - 1 < Math.pow(n, 2); i--) {
                 result[down][i] = value++;
             }
             down--;
-            for (int i = down; i >= up && value - 1 <  Math.pow(n,2); i--) {
+            for (int i = down; i >= up && value - 1 < Math.pow(n, 2); i--) {
                 result[i][left] = value++;
             }
             left++;
