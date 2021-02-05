0f09767eee93b8ef8038996c6dc3864325c973c0
sunchen <sunchen@trawe.cn>
2021-02-06 02:17:52 +0800

    回文素数

--- src/main/java/com/algorithm/normal/m866/PrimePalindrome.java
+++ src/main/java/com/algorithm/normal/m866/PrimePalindrome.java
@@ -6,13 +6,13 @@ package com.algorithm.normal.m866;
  * 回顾一下，如果一个数大于 1，且其因数只有 1 和它自身，那么这个数是素数。
  * 例如，2，3，5，7，11 以及13 是素数。
  * 回顾一下，如果一个数从左往右读与从右往左读是一样的，那么这个数是回文数。
- *
  * 例如，12321 是回文数。
+ *
  * @author sunchen
  * @date 2021/2/5 11:58 下午
  */
 public class PrimePalindrome {
-    public int primePalindrome(int n) {
+    public static int primePalindrome(int n) {
         n = getNextPalindrome(n);
         while (!isPrime(n)) {
             n++;
@@ -21,28 +21,30 @@ public class PrimePalindrome {
         return n;
     }
 
-    public int getNextPalindrome(int n) {
+    public static int getNextPalindrome(int n) {
         //1.先反转字符串
-        char[] s = String.valueOf(n).toCharArray();
-        int mid = s.length/2;
-        while (true) {
-            for (int i = 0; i < mid; i++) {
-                s[s.length - 1 - i] = s[i];
-            }
-            int tmp = Integer.parseInt(String.valueOf(s));
-            if (tmp >= n) return tmp; // 如果>=n，返回这个造出来的数
-                // 如果比n小，那前半段+1
-            else {
-                int j = s.length % 2 == 0 ? mid-1 : mid;
-                while (s[j] == '9') {
-                    s[j--] = '0';
-                }
-                s[j]++;
-            }
+        String string = getPalindrome(n);
+        int result = Integer.parseInt(string);
+        if (result >= n) {
+            return result;
         }
+        //把前半部分值加1
+        int mid = (int) Math.ceil(string.length() / 2.0);
+        //转成回文，返回
+        n = Integer.parseInt(string.substring(0, mid)) + 1;
+        return Integer.parseInt(getPalindrome(Integer.parseInt(n + string.substring(mid))));
     }
 
-    public boolean isPrime(int n) {
+    public static String getPalindrome(int n) {
+        char[] s = String.valueOf(n).toCharArray();
+        int mid = s.length / 2;
+        for (int i = 0; i < mid; i++) {
+            s[s.length - 1 - i] = s[i];
+        }
+        return String.valueOf(s);
+    }
+
+    public static boolean isPrime(int n) {
         if (n == 1) {
             return false;
         }
@@ -52,5 +54,9 @@ public class PrimePalindrome {
             }
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(primePalindrome(13));
     }
 }
